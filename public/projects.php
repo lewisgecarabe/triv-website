8e7b0ee1c048edadbccabaa92e292c2abca228b6
noelthegreatestdeveloper <noelandraevillanueva13@gmail.com>
2025-05-18 18:32:27 +0800

    a

---
+++ public/projects.php
@@ -0,0 +1,34 @@
+<?php include 'functions.php'; ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>TRIV Design & Construction</title>
+    <link rel="stylesheet" href="../assets/css/public-style.css">
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300&display=swap" rel="stylesheet">
+</head>
+<body>
+    <header>
+        <div class="logo">
+            <img src="../assets/images/triv-logo.png" alt="TRIV Design & Construction">
+        </div>
+        <nav>
+            <ul>
+                <li><a href="../public/index.php">HOME</a></li>
+                <li><a href="../public/services.php">SERVICES</a></li>
+                <li><a href="../public/developers.php">DEVELOPERS</a></li>
+                <li><a href="../public/contact.php">CONTACT US</a></li>
+                  <li><a href="../public/career.php">CAREERS</a></li>
+                  <li><a href="../public/projects.php">PROJECTS</a></li>
+            </ul>
+        </nav>
+    </header>
+
+    <section class="hero developers-hero">
+        <div class="hero-overlay"></div>
+        <img src="../assets/images/codings.jpg" alt="Code Background" class="hero-bg">
+        <div class="hero-content">
+            <h1>FOUNDERS</h1>
+        </div>
+    </section>
